383993a23730ccbb1ad389c215ed4f6da7d32345
rojojun <zoloman316@gmail.com>
2022-12-14 22:54:23 +0900

    feat> ...을 활용한 arraycopy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 function App() {
+  let [postTitle, setPostTitle] = useState([
+    "남자 코트 추천",
+    "ㅇㅇㅇ",
+    "살 빼자 ㅠㅠㅠ",
+  ]);
+  let [likeCnt, setLikeCnt] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>Blog</h4>
+      </div>
+      <div className="list">
+        <h4>
+          {postTitle[0]}
+          <span
+            onClick={() => {
+              setLikeCnt(likeCnt++);
+              alert("좋아요를 합니다.");
+            }}
+          >
+            {" "}
+            ❤️{" "}
+          </span>{" "}
+          {likeCnt}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{postTitle[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{postTitle[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <button
+        onClick={() => {
+          let copyPostTitle = [...postTitle];
+          copyPostTitle[0] = "여자 코트 추천";
+          setPostTitle(copyPostTitle);
+        }}
+      >
+        여자코트를 원해요?
+      </button>
     </div>
   );
 }
