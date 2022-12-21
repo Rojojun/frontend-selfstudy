3853bd8b98c14ae94cf9d31e45fc6c6a5a3e0187
rojojun <zoloman316@gmail.com>
2022-12-21 21:24:28 +0900

    git 이관 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,69 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+// img html에 넣을 경우
+import test_img from "./img/bg.png";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Hojun's Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Item</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      {/* 위랑 같은 의미
+      <div
+        className="main-test"
+        style={{ backgroundImage: "url(" + test_img + ")" }}
+      ></div> */}
+
+      <Container>
+        <Row>
+          <Col sm>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            {/*
+            <img src={process.env.PUBLIC_URL + "/logo192.png"} /> 
+            public 폴더 이미지에 넣을 때 권장하는 방식
+            */}
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col sm>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col sm>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
