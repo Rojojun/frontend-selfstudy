1dd3dab3e0decbbabdd57104ab819c47abf56ce7
rojojun <zoloman316@gmail.com>
2022-12-29 20:31:50 +0900

    동적으로 작동

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,20 +2,17 @@ import { useParams } from "react-router-dom";
 
 function Detail(props) {
   let { id } = useParams();
-
+  let item = props.shoes.find((i) => i.id == id);
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
-          <img
-            src="https://codingapple1.github.io/shop/shoes1.jpg"
-            width="100%"
-          />
+          <img src={item.pictureInfo} width="100%" />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}원</p>
+          <h4 className="pt-5">{item.title}</h4>
+          <p>{item.content}</p>
+          <p>{item.price}원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
